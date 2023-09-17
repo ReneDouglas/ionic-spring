13a68dc84835fbf06199d947984b3b2b5a2344d8
Rene Morais <rene.douglas.bsi@gmail.com>
2023-09-17 18:11:38 -0300

    commit inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.spring',
+  appName: 'ionic-spring',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
